e207c2fcd14977bc10b641abd14b1202c7febfa5
Ubuntu <ubuntu@ip-172-31-27-44.us-west-2.compute.internal>
2024-06-28 03:02:07 +0000

    first commit

--- README.md
+++ README.md
@@ -68,3 +68,4 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+# CIS459_Module_Two_Hardhat_Voting_SUMM2024
--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import Web3 from 'web3'
+import VotingAbi from './contractsData/Voting.json'
+import VotingAddress from './contractsData/Voting-address.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    componentWillMount(){
+        this.loadBlockchainData()
+    }
+
+async loadBlockchainData(){
+    console.log('VotingAddress.address', VotingAddress.address)
+    console.log('VotingAbi.abi', VotingAbi.abi)
+    let NewHelloAbi = require('./contractsData/Voting.json');
+    const web3 = new Web3(new Web3.providers.HttpProvider("http://ec2-35-164-183-115.us-west-2.compute.amazonaws.com:8545"))
+    var account;
+    const accounts  = await web3.eth.getAccounts()
+    console.log(accounts)
+    web3.eth.getAccounts().then((f) => {
+        account = f[0];
+    })
+
+    this.setState( { account : accounts[0] })
+    console.log(account);
+    const contract = new web3.eth.Contract(VotingAbi.abi);
+    contract.options.address = VotingAddress.address
+    this.setState( { contract })
+    let myCandidateList = await  contract.methods.getCandidateList().call();  
+    console.log(myCandidateList);   
+//const number = await contract.methods.totalVotesFor('Johnny').call(console.log)
+  //  console.log("Johnny number: ", number)    
+    var count = await this.state.contract.methods.totalVotesFor('Johnny').call(console.log);
+    
+        this.setState( { Johnny: parseInt(count) } )
+
+
+	count = await this.state.contract.methods.totalVotesFor('Amber').call(console.log);
+
+        this.setState( { Amber : parseInt(count) } )
+
+}
+    constructor(props){
+        super(props)
+        console.log("constructor")
+        this.state = {
+            account: '',
+            loading: true,
+            message: ''
+        }
+     this.voting = this.voting.bind(this)
+
+    }
+    voting = async (name) =>  {
+        this.state.contract.methods.voteForCandidate(name).send({gas: 140000, from: this.state.account });
+        var count = await this.state.contract.methods.totalVotesFor(name).call(console.log);
+        this.setState( { [name]: parseInt(count) } )
+ }
+
+
+render(){
+
+        return (
+                <div>
+                <div>
+                 <button onClick={() => this.voting("Johnny")}>Johnny vote</button>
+                 <button onClick={() => this.voting("Amber")}>Amber vote</button>
+
+		</div>
+
+                <h5>Johnny count: {this.state.Johnny}</h5>
+                <h5>Amber count: {this.state.Amber}</h5>
+
+		</div>
+        );
+}
 }
 
 export default App;
